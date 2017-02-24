e699e0a9052e0b5be5d18da83580194e01e63009
Alexander Rokven <info@alexanderrokven.nl>
2017-02-24 01:50:29 +0100

    Added day agenda

--- resources/views/pages/dayagenda.blade.php
+++ resources/views/pages/dayagenda.blade.php
@@ -1,4 +1,151 @@
 @extends('layouts.default')
 @section('content')
-  i am the day agenda page
+    <h1>Welder</h1>
+    <div class="job-maps">
+      <img class="maps" src="img/zrpoint.png" />
+
+    </div>
+    <div class="job-info">
+      <p>
+        Location: Zeeland Refinery - East
+      </p>
+      <p>
+        Start date: January 2017
+      </p>
+      <p>
+        End date: March 2017
+      </p>
+      <p>
+        Contractor: High Zealand Pipelines
+      </p>
+      <p>
+        Permits: VCA
+      </p>
+    </div>
+    <div class="lined">
+      <table>
+        <h2>Work Schedule</h2>
+        <tr class="bold">
+          <td>
+            Day
+          </td>
+          <td>
+            Begin Time
+          </td>
+          <td>
+            End Time
+          </td>
+          <td>
+            Rest
+          </td>
+          <td>
+            Project
+          </td>
+          <td>
+            To Do
+          </td>
+        </tr>
+        <tr>
+          <td>
+            12 January
+          </td>
+          <td>
+            08:00
+          </td>
+          <td>
+            16:00
+          </td>
+          <td>
+            01:00
+          </td>
+          <td>
+            Hydrocracker
+          </td>
+          <td>
+            Place a scaffold
+          </td>
+        </tr>
+        <tr>
+          <td>
+            11 January
+          </td>
+          <td>
+            08:00
+          </td>
+          <td>
+            16:00
+          </td>
+          <td>
+            01:00
+          </td>
+          <td>
+            Hydrocracker
+          </td>
+          <td>
+            Remove a scaffold
+          </td>
+        </tr>
+        <tr>
+          <td>
+            10 January
+          </td>
+          <td>
+            08:00
+          </td>
+          <td>
+            16:00
+          </td>
+          <td>
+            01:00
+          </td>
+          <td>
+            Hydrocracker
+          </td>
+          <td>
+            Place a scaffold
+          </td>
+        </tr>
+        <tr>
+          <td>
+            09 January
+          </td>
+          <td>
+            08:00
+          </td>
+          <td>
+            16:00
+          </td>
+          <td>
+            01:00
+          </td>
+          <td>
+            Hydrocracker
+          </td>
+          <td>
+            Remove a scaffold
+          </td>
+        </tr>
+        <tr>
+          <td>
+            08 January
+          </td>
+          <td>
+            08:00
+          </td>
+          <td>
+            16:00
+          </td>
+          <td>
+            01:00
+          </td>
+          <td>
+            Hydrocracker
+          </td>
+          <td>
+            Place a scaffold
+          </td>
+        </tr>
+      </table>
+    </div>
+
 @stop
